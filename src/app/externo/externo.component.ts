69c24c76cd51d1b2a13b446f673398e7a88093ac
DXelor <dzaliaspro@gmail.com>
2020-11-18 00:31:48 -0500

    Formularios - AJAX & HTTP Post

--- src/app/externo/externo.component.ts
+++ src/app/externo/externo.component.ts
@@ -14,10 +14,17 @@ export class ExternoComponent implements OnInit {
   public userId: any;
   public fecha: any;
 
+  public new_user: any;
+  public usuario_guardado: any;
+
   constructor(
     private _peticionesService: PeticionesService
   ) {
     this.userId = 1;
+    this.new_user = {
+      "name":"",
+      "job":""
+    };
   }
 
   ngOnInit(){
@@ -35,6 +42,13 @@ export class ExternoComponent implements OnInit {
         console.log(<any>error);
       }
     );
+  }
+
+  onSubmit(form){
+    this._peticionesService.addUser(this.new_user).subscribe(
+      response => this.usuario_guardado = response,
+      error => console.log(<any>error)
+    )
   }
 
 }
