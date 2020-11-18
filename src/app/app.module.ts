6bfab3f37529cf896e024c177688015b4a7b8a8c
DXelor <dzaliaspro@gmail.com>
2020-11-17 22:36:57 -0500

    crear formularios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { HomeComponent }       from './home/home.component';
 import { ExternoComponent }    from './externo/externo.component';
 
 import { CalculadoraPipe } from './pipes/calculadora.pipe';
+import { ContactoComponent } from './contacto/contacto.component';
 
 
 @NgModule({
@@ -25,7 +26,8 @@ import { CalculadoraPipe } from './pipes/calculadora.pipe';
     CursosComponent,
     HomeComponent,
     ExternoComponent,
-    CalculadoraPipe
+    CalculadoraPipe,
+    ContactoComponent
   ],
   imports: [
     CommonModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -9,6 +9,7 @@ import { ZapatillasComponent } from './zapatillas/zapatillas.component';
 import { VideojuegoComponent } from './Videojuego/videojuego.component';
 import { CursosComponent }     from './cursos/cursos.component';
 import { ExternoComponent }    from './externo/externo.component';
+import { ContactoComponent }   from './contacto/contacto.component';
 
 
 
@@ -21,6 +22,7 @@ const appRoutes: Routes = [
     {path: 'cursos',            component: CursosComponent},
     {path: 'cursos/:nombre',    component: CursosComponent},
     {path: 'externo',           component: ExternoComponent},
+    {path: 'contacto',          component: ContactoComponent},
     {path: '**',                component: HomeComponent}
 ];
 
